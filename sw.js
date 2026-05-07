281f7e774a4929a980a6f98eb80e6d9cee3bb516
lhcwwangxc <15141914419@163.com>
2026-05-07 23:18:19 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'ludi-poetry-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if (resp.ok && e.request.url.includes('cdnjs.cloudflare.com')) {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return resp;
+    }))
+  );
+});
