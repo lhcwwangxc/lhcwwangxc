4cb717e04c526dab50da617b407b62c0443c2aed
lhcwwangxc <15141914419@163.com>
2026-05-08 09:21:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,11 +5,9 @@ const ASSETS = [
   './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
